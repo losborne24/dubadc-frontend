db4a7b56b1c634ed35ccbfe00746ab8aa9668133
Leith <osborneleith@gmail.com>
2020-09-01 17:07:55 +0100

    created templates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,11 +7,6 @@ import "../stylesheets/css/home.css"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
--- src/pages/news.js
+++ src/pages/news.js
@@ -1,16 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "../stylesheets/css/news.css"
 
-const SecondPage = () => (
+const IndexPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="News" />
   </Layout>
 )
 
-export default SecondPage
+export default IndexPage
